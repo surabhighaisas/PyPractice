d03a2509cd32e2af8cfcb5dd7e806f360acdb8ad
surabhighaisas <53860782+surabhighaisas@users.noreply.github.com>
2025-07-08 13:13:21 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,5 +1,6 @@
 # Detect if given integer is palindrome
 
+# With string conversion
 def is_palindrome(n):
     str_n = str(n)
     reverse_str_n = str_n[::-1]
@@ -7,8 +8,19 @@ def is_palindrome(n):
         return True
     else:
         return False
-    
+
+# Without string conversion    
+def is_palindrome_without_string_conversion(n):
+    temp = n
+    reverse = 0
+    while(temp != 0):
+        reverse = (reverse * 10) + (temp % 10)
+        temp = temp // 10
+    if reverse == n:
+        return True
+    else:
+        return False
 
 print(is_palindrome(12345))
 print(is_palindrome(123454321))
-print(is_palindrome(11011))
+print(is_palindrome_without_string_conversion(11011))
